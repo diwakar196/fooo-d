1ecdcbaef37d723eb8b8fcb8e57730814e28edf8
Diwakar Singh <64231526+diwakar196@users.noreply.github.com>
2022-03-25 23:50:49 +0530

    Adding Frontend Files

---
+++ Frontend/src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div>
+        <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
